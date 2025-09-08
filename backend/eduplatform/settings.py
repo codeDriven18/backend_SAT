473dc47d5d5fe0b435c20604cc56182ab477ca47
codeDriven18 <quronboyevferuzbek0@gmail.com>
2025-09-09 03:22:22 +0500

    host update

--- backend/eduplatform/settings.py
+++ backend/eduplatform/settings.py
@@ -137,6 +137,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3001",  # Teacher frontend
     "http://127.0.0.1:3000",
     "http://127.0.0.1:3001",
+    "https://backend-sato.onrender.com/"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
