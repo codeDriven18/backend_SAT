c03d72d27b3fe76673cf24f63f8d086edfab5d1d
codeDriven18 <quronboyevferuzbek0@gmail.com>
2025-09-09 04:29:04 +0500

    _____________

--- backend/eduplatform/settings.py
+++ backend/eduplatform/settings.py
@@ -133,17 +133,17 @@ SIMPLE_JWT = {
 
 # CORS settings
 
-# ALLOWED_HOSTS = ['localhost', '127.0.0.1', ]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'backend-sato.onrender.com']
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # Student frontend
-    "http://localhost:3001",  # Teacher frontend
-    "http://127.0.0.1:3000",
-    "http://127.0.0.1:3001",
-    'https://backend-sato.onrender.com',
-    'http://backend-sato.onrender.com',
-    'backend-sato.onrender.com'
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",  # Student frontend
+#     "http://localhost:3001",  # Teacher frontend
+#     "http://127.0.0.1:3000",
+#     "http://127.0.0.1:3001",
+#     'https://backend-sato.onrender.com',
+#     'http://backend-sato.onrender.com',
+#     'backend-sato.onrender.com'
+# ]
 
 CORS_ALLOW_CREDENTIALS = True
 
