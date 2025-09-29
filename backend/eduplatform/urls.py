25059b09aa32af0cba31a66c631609faa60057db
codeDriven18 <quronboyevferuzbek0@gmail.com>
2025-09-29 15:35:39 +0500

    update

--- backend/eduplatform/urls.py
+++ backend/eduplatform/urls.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
-from streamlit import status
+# from streamlit import status
 from django.conf.urls.static import static
 
 
