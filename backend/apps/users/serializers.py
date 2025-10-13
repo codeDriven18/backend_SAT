0d803b693bcd12c4186669d1f22c8badf4549261
Feruzbek Kuranboev <quronboyevferuzbek0@gmail.com>
2025-10-13 21:34:44 +0500

    fixed_

--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -84,3 +84,11 @@ class TeacherProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = TeacherProfile
         fields = '__all__'
+
+class DashboardStatsSerializer(serializers.Serializer):
+    total_tests = serializers.IntegerField()
+    avg_score = serializers.FloatField()
+    best_score = serializers.FloatField()
+    worst_score = serializers.FloatField()
+    recent_tests = serializers.ListField()
+    performance_chart = serializers.ListField()
