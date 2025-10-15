67ebd3a30fb978c8fa8a8489f67bb719b8f9de9a
Feruzbek Kuranboev <quronboyevferuzbek0@gmail.com>
2025-10-15 17:28:18 +0500

    config

--- backend/eduplatform/settings.py
+++ backend/eduplatform/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'eduplatform.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
+        default=config('DATABASE_URL'),
         conn_max_age=600,
         ssl_require=True
     )
