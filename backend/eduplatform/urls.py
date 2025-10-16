395c119224278a8450bfe94fae1c28c5c0375f4f
Feruzbek Kuranboev <quronboyevferuzbek0@gmail.com>
2025-10-16 15:56:39 +0500

    auth

--- backend/eduplatform/urls.py
+++ backend/eduplatform/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,6 +27,11 @@ urlpatterns = [
 
 ]
 
+    urlpatterns = [
+        path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+        path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+]
 # Serve media files (images, uploads) during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
