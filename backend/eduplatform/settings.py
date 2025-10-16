1794f179f448ea550fb80f81a85d81dba0516d68
Feruzbek Kuranboev <quronboyevferuzbek0@gmail.com>
2025-10-16 15:47:25 +0500

    auth

--- backend/eduplatform/settings.py
+++ backend/eduplatform/settings.py
@@ -127,7 +127,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_SCHEMA_CLASS': 'eduplatform.schema.CustomAutoSchema',  # Add this line
     'DEFAULT_FILTER_BACKENDS': [
