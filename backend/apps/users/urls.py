239c7c5eb4e9e85e3189eb7638b35ba1f8e6c92a
Feruzbek Kuranboev <quronboyevferuzbek0@gmail.com>
2025-10-16 15:52:29 +0500

    auth

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,12 +1,12 @@
-from django.urls import path
-from .views import RegisterView, LoginView, ProfileView
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # <-- add this
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('me/', ProfileView.as_view(), name='me'),
-    # alias expected by frontend
     path('profile/', ProfileView.as_view(), name='profile'),
 ]
+
+    # path('profile/picture/', ProfilePictureUploadView.as_view(), name='profile-picture-upload'),
